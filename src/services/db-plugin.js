9ed96090bdb3d89325caf744d92a6d3122f5816a
Sebastian Castro <sebastian.castro@protonmail.com>
2022-01-12 16:27:52 +0100

    db-plugin: toast success

--- src/services/db-plugin.js
+++ src/services/db-plugin.js
@@ -11,9 +11,9 @@ export default {
           const { data, error } = await this.$db.from(dbName).insert([object]).single()
           if (error) this.toastError(error)
           else {
-            if (dbName === 'sessions') data.events.forEach((e) => { e.start_date = new Date(e.start_date) })
+            if (dbName === 'sessions') (data.events || []).forEach((e) => { e.start_date = new Date(e.start_date) })
             this.$root[dbName][data.id] = data
-
+            this.toastSuccess(data, 'created')
             if (onSuccess) onSuccess(data)
           }
           this.loading = false
@@ -31,6 +31,7 @@ export default {
             if (data.delivery_date) data.delivery_date = new Date(data.delivery_date)
             if (data.target_date) data.target_date = new Date(data.target_date)
             this.$root[dbName][object.id] = data
+            this.toastSuccess(data, 'updated')
           }
           this.loading = false
         },
@@ -48,6 +49,12 @@ export default {
           if (typeof error === 'string') error = { message: 'Error', details: error }
           this.$toast.add({
             severity: 'error', summary: error.message, detail: error.details, life: 8000,
+          })
+        },
+        toastSuccess(object, action) {
+          const message = object.name ? `${object.name} successfully ${action}` : `Successfully ${action}`
+          this.$toast.add({
+            severity: 'success', summary: 'Success', detail: message, life: 8000,
           })
         },
       },
