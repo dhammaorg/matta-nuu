a341db67a9bf064d0035bc68c08f1963f566f0a5
Sebastian Castro <sebastian.castro@protonmail.com>
2022-02-02 08:23:28 +0100

    Recipie: Adds "prepare day before" option

--- src/views/recipies/RecipieForm.vue
+++ src/views/recipies/RecipieForm.vue
@@ -5,19 +5,21 @@
       <InputText id="name" v-model.trim="recipie.name" required="true" placeholder="Name" autofocus/>
     </div>
 
-    <div class="fw-bold my-3">
-      <span>Ingredients for</span>
-      <InputNumber v-model="recipie.people_count" class="d-inline-block w-auto mx-2"
-                   inputStyle="width: 4rem" inputClass="text-center"/>
-      <span>People</span>
-    </div>
+    <div class="ingredients my-4 py-2">
+      <div class="fw-bold mb-3">
+        <span>Ingredients for</span>
+        <InputNumber v-model="recipie.people_count" class="d-inline-block w-auto mx-2"
+                    inputStyle="width: 4rem" inputClass="text-center"/>
+        <span>People</span>
 
-    <div class="mb-3">
+        <Button icon="pi pi-plus" class="p-button-rounded p-button-primary p-button-sm float-end"
+              @click="recipie.products.push({})"></Button>
+      </div>
       <div v-for="product in recipie.products" class="d-flex mb-2" :key="product">
         <div class="p-inputgroup">
           <InputProduct v-model="product.id" />
           <InputNumber v-model="product.amount" :maxFractionDigits="5" placeholder="Amount"
-                       inputClass="border-start-0" />
+                        inputClass="border-start-0" />
           <span class="p-inputgroup-addon" style="width: 5rem;">{{ $root.getProduct(product.id).unit }}</span>
         </div>
         <Button icon="pi pi-times" class="p-button-text p-button-danger"
@@ -25,8 +27,10 @@
       </div>
     </div>
 
-    <Button icon="pi pi-plus" class="p-button-rounded p-button-primary"
-            @click="recipie.products.push({})"></Button>
+    <div class="field-checkbox">
+      <Checkbox id="day_before" v-model="recipie.prepare_day_before" :binary="true" />
+      <label for="day_before" class="ms-2">Need to be prepared the day before</label>
+    </div>
 
     <template #footer>
       <Button label="Cancel" icon="pi pi-times" class="p-button-text" @click="visible = false"/>
@@ -38,10 +42,11 @@
 <script>
 
 import InputNumber from 'primevue/inputnumber'
+import Checkbox from 'primevue/checkbox'
 import InputProduct from '@/components/InputProduct.vue'
 
 export default {
-  components: { InputNumber, InputProduct },
+  components: { InputNumber, InputProduct, Checkbox },
   data() {
     return {
       visible: false,
@@ -58,6 +63,9 @@ export default {
     },
     async saveRecipie() {
       if (this.recipie.name) {
+        // filter empty ingredients
+        this.recipie.products = this.recipie.products.filter((p) => p.id && p.amount)
+
         if (this.recipie.id) {
           this.dbUpdate('recipies', this.recipie)
         } else {
@@ -69,7 +77,7 @@ export default {
       }
     },
     removeProduct(product) {
-      this.recipie.products = this.recipie.products.filter((p) => p.name !== product.name)
+      this.recipie.products = this.recipie.products.filter((p) => p.id !== product.id)
     },
   },
 }
