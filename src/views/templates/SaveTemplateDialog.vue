99dbfa2abf8baac1b364895ec82722b818606bfd
Sebastian Castro <sebastian.castro@protonmail.com>
2022-02-21 15:25:10 +0100

    Fix save event as template

--- src/views/templates/SaveTemplateDialog.vue
+++ src/views/templates/SaveTemplateDialog.vue
@@ -35,7 +35,37 @@ export default {
       template: {},
     }
   },
-  computed: {
+  methods: {
+    show(event = {}) {
+      this.template = { ...event }
+      this.visible = true
+    },
+    save() {
+      const existingTemplate = this.$root.templatesArray.find((t) => t.name == this.template.name)
+      if (existingTemplate) {
+        this.confirmOveride('A template with that name already exists, would you like to override it?', existingTemplate.id)
+      } else {
+        const newTemplate = this.sessionTemplate()
+        delete newTemplate.id // ensure id is null
+        this.dbCreate('sessions', newTemplate, () => { this.visible = false })
+      }
+    },
+    overrideTemplate(e) {
+      this.template.name = e.data.name
+      this.confirmOveride('Are you sure to override this template?', e.data.id)
+    },
+    confirmOveride(message, templateId) {
+      const newTemplate = this.sessionTemplate()
+      newTemplate.id = templateId
+      this.$confirm.require({
+        message,
+        header: 'Confirmation',
+        icon: 'pi pi-exclamation-triangle',
+        accept: async () => {
+          this.dbUpdate('sessions', newTemplate, () => { this.visible = false })
+        },
+      })
+    },
     // We encapsulate the eventTemplate inside a session in order to facilitate storage and usage
     sessionTemplate() {
       return {
@@ -45,50 +75,23 @@ export default {
         is_template: true,
       }
     },
-  },
-  methods: {
-    show(event = {}) {
-      this.template = { ...event }
-      this.visible = true
-    },
-    save() {
-      const existingTemplate = this.$root.templatesArray.find((t) => t.name == this.template.name)
-      if (existingTemplate) {
-        this.template.id = existingTemplate.id
-        this.confirmOveride('A template with that name already exists, would you like to override it?')
-      } else {
-        delete this.template.id // ensure id is null
-        this.dbCreate('sessions', this.sessionTemplate, () => { this.visible = false })
-      }
-    },
-    overrideTemplate(e) {
-      this.template.id = e.data.id
-      this.template.name = e.data.name
-      this.confirmOveride('Are you sure to override this template?')
-    },
-    confirmOveride(message) {
-      this.$confirm.require({
-        message,
-        header: 'Confirmation',
-        icon: 'pi pi-exclamation-triangle',
-        accept: async () => {
-          this.dbUpdate('sessions', this.sessionTemplate, () => { this.visible = false })
-        },
-      })
-    },
     // When we save a template from a standard session where there are multiple events,
     // we filter the session rows associated with this particular event
     getEventRows(event) {
       const result = []
       const eventId = `Event${event.id}_`
       this.$root.session.rows.forEach((row) => {
-        const values = []
+        const values = {}
+        let dayIndex = 0
         // filters values associated to this event
         Object.entries(row.values).forEach(([key, value]) => {
-          if (key.includes(eventId)) values.push(value)
+          if (key.includes(eventId)) {
+            values[`Event1_${dayIndex}`] = value
+            dayIndex += 1
+          }
         })
-        // If this row is used by the event (i.e. there is at least on value for this row in this event)
-        if (values.find((v) => Object.values(v).length > 0)) result.push({ ...row, ...{ values } })
+        // If this row is used by the event (i.e. there is at least one value for this row in this event)
+        if (Object.values(values).find((v) => Object.values(v).length > 0)) result.push({ ...row, ...{ values } })
       })
       return result
     },
