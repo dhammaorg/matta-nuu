5812453d242ede02bad621032c3c346e3aea604a
Sebastian Castro <sebastian.castro@protonmail.com>
2022-08-25 12:49:23 +0200

    Fix auto scrolling to today's date

--- src/services/calendar-mixin.js
+++ src/services/calendar-mixin.js
@@ -10,9 +10,9 @@ export default {
     setTimeout(() => { this.isMounted = true }, 0)
   },
   updated() {
-    if (this.firstUpdate) {
+    const today = document.querySelector('.today')
+    if (this.firstUpdate && today) {
       // Scroll to today
-      const today = document.querySelector('.today')
       const wrapper = document.querySelector('.p-datatable-wrapper')
       const topLeftCell = document.querySelector('.top-left-cell')
       if (today) wrapper.scrollLeft = today.offsetLeft - topLeftCell.offsetWidth
