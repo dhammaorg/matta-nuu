c8b93ca3c0986502d5cede1fdd891580c471e9d2
Sebastian Castro <sebastian.castro@protonmail.com>
2024-02-04 15:49:15 +0100

    inventory: simplify logic to choose next Area. Save completed areas

--- src/views/sessions/SessionInventory.vue
+++ src/views/sessions/SessionInventory.vue
@@ -1,22 +1,50 @@
 <template>
   <div class="d-flex flex-column h-100 pb-4 mx-auto justify-content-between"
        style="max-width: 600px; max-height: 800px;">
+
+    <!-- Header -->
     <div class="header">
       <div class="d-flex">
         <h1 class="m-0"><span class="d-none d-md-inline">Inventory - </span>{{ inventory.day_label }}
         </h1>
-        <Button v-if="!finished" icon="pi pi-save" title="Save"
+        <Button v-if="currentStorageArea" icon="pi pi-save" title="Save"
                 class="p-button-success ms-auto"
                 @click="save" :loading="saving" />
       </div>
-      <h3 v-if="currentStorageArea" :class="{ 'text-primary': storageState === 'chooseNext' }">
-        <i class=" me-2 fs-5"
-           :class="storageState === 'chooseNext' ? 'pi pi-check-circle' : 'pi pi-map-marker'"></i>
+      <h3 v-if="currentStorageArea">
+        <i class=" me-2 fs-5 pi pi-map-marker"></i>
         {{ currentStorageArea.name }}
       </h3>
     </div>
 
-    <template v-if="storageState == 'inProgress' && inventory.values[currentProduct.id]">
+    <!-- Choose Area -->
+    <template v-if="!currentStorageArea">
+      <div class="content">
+        <h3 class="text-primary">
+          <span v-if="allAreasCompleted">Inventory done !</span>
+          <span v-else-if="noneAreaCompleted">Choose the Area to Start</span>
+          <span v-else>Choose the Next Area</span>
+        </h3>
+        <div v-for="area in storageAreas" :key="area.id"
+             @click="startArea(area)"
+             class="storage-area-card border mb-3 rounded-3 p-3"
+             :class="{ completed: isAreaCompleted(area) }">
+          <i v-if="isAreaCompleted(area)" class="pi-check-circle pi me-2"></i>
+          <i v-else class="pi-circle pi me-2"></i>
+          {{ area.name }}
+          <span class="float-end">
+            {{ completedProductsForArea(area).length }} / {{ productsForArea(area).length }}
+          </span>
+        </div>
+      </div>
+      <div class="footer">
+        <Button label="Save" class="w-100 p-button-success" @click="save" :loading="saving" />
+      </div>
+    </template>
+
+    <!-- Fill Area Stocks -->
+    <template v-if="currentStorageArea && inventory.values[currentProduct.id]">
+      <!-- Stock & Unit inputs -->
       <div class="content">
         <h3 class="mb-4">{{ currentProduct.name }}</h3>
         <div class="p-inputgroup">
@@ -24,14 +52,15 @@
                        placeholder="Stock" ref="stockInput"
                        @keyup.enter="onInputStockKeyEnter" />
           <!-- <span class="p-inputgroup-addon" style="width: 5rem;" v-if="day"
-              v-tooltip.top="'Theoretical stock'">
-          {{ stockValueFor(stock.product_id) }}
-        </span> -->
+              v-tooltip.top="'Theoretical stock'">{{ stockValueFor(stock.product_id) }}
+          </span> -->
           <span class="p-inputgroup-addon" style="width: 5rem;">
             {{ inventory.values[currentProduct.id].unit }}
           </span>
         </div>
       </div>
+
+      <!-- Navigation -->
       <div class="footer d-flex align-items-center justify-content-between">
         <Button label="Prev" icon="pi pi-chevron-left" class="p-button-secondary"
                 @click="productIndex = Math.max(productIndex - 1, 0)"
@@ -40,93 +69,57 @@
 
         <Button v-if="productIndex == currentProducts.length - 1"
                 label="Finish"
-                @click="finishArea" ref="nextButton" />
+                @click="finishCurrentArea" ref="nextButton" />
         <Button v-else label="Next" icon="pi pi-chevron-right"
                 @click="productIndex = productIndex + 1" ref="nextButton" />
       </div>
-    </template>
-
-    <template v-if="storageState == 'chooseNext'">
-      <template v-if="finished">
-        <div class="content">
-          <h1>Inventory is finished !</h1>
-        </div>
-        <div class="footer">
-          <Button icon="pi pi-save" label="Save" class="p-button-success"
-                  @click="save" :loading="saving" />
-        </div>
-      </template>
-      <template v-else>
-        <div class="content">
-          <div class="p-field">
-            <label>{{ currentStorageArea ? 'Next Storage is' : 'Choose the area to start' }}</label>
-            <InputCategory type="StorageArea" v-model="nextStorageAreaId" :list="nextStorageAreas"
-                           :btnAdd="false" :showClear="false" />
-          </div>
-        </div>
-        <div class="footer">
-          <Button label="Continue" class="w-100"
-                  @click="currentStorageAreaId = nextStorageAreaId" />
-        </div>
-      </template>
     </template>
   </div>
 </template>
 
 <script>
-import Card from 'primevue/card'
 import InputNumber from 'primevue/inputnumber'
 import StockMixin from '@/services/stocks-mixin'
-import InputCategory from '@/components/InputCategory.vue'
 
 export default {
   inject: ['sessionDays', 'stockDays', 'sessionInventories'],
   mixins: [StockMixin],
-  components: { Card, InputNumber, InputCategory },
+  components: { InputNumber },
   data() {
     return {
       inventory: {
         storage_area_ids: [],
         product_category_ids: [],
         supplier_ids: [],
+        completed_storage_areas_ids: [],
         values: {},
       },
-      completedStorageAreaIds: [],
-      currentStorageAreaId: undefined,
-      nextStorageAreaId: undefined,
-      storageState: '',
+      otherArea: { name: 'Other Products', id: 'other' },
+      currentStorageArea: undefined,
       productIndex: 0,
       saving: false,
     }
   },
   beforeMount() {
     this.inventory = this.sessionInventories.find((inv) => inv.id == this.$route.params.inventory_id)
-    this.nextStorageAreaId = this.nextStorageAreas.at(0)?.id
-    if (this.storageAreas.length === 1) {
-      this.currentStorageAreaId = this.storageAreas.at(0)
-      this.storageState = 'inProgress'
-    } else {
-      this.storageState = 'chooseNext'
-    }
+    if (this.storageAreas.length === 1) this.currentStorageArea = this.storageAreas.at(0)
   },
   computed: {
     storageAreas() {
-      let result = Object.values(this.$root.categories).filter((c) => c.type === 'StorageArea')
+      let result = Object.values(this.$root.categories)
+        .filter((c) => c.type === 'StorageArea')
+        .sort((a, b) => a.name.localeCompare(b.name))
+
       if (this.inventory.storage_area_ids.length > 0) {
         result = result.filter((c) => this.inventory.storage_area_ids.includes(c.id))
       } else {
-        result.push({ name: 'Other Products', id: 'other' })
+        result.push(this.otherArea)
       }
 
       return result
     },
-    currentStorageArea() {
-      if (this.storageAreas.length === 1) return this.storageAreas.at(0)
-
-      return this.storageAreas.find((area) => this.currentStorageAreaId == area.id)
-    },
-    nextStorageAreas() {
-      return this.storageAreas.filter((area) => !this.completedStorageAreaIds.includes(area.id))
+    isAreaCompleteds() {
+      return this.storageAreas.filter((area) => this.inventory.completed_storage_areas_ids.includes(area.id))
     },
     products() {
       let result = this.sessionProducts.map((productId) => this.$root.getProduct(productId))
@@ -138,26 +131,41 @@ export default {
       }
       return result
     },
+    // Products related to currentStorageArea
     currentProducts() {
-      if (!this.currentStorageArea) return []
-
-      return this.products.filter((product) => {
-        if (this.currentStorageArea.id === 'other') return product.storage_area_ids.length == 0
-        return product.storage_area_ids.includes(this.currentStorageArea.id)
-      })
+      return this.currentStorageArea ? this.productsForArea(this.currentStorageArea) : []
     },
     currentProduct() {
       return this.currentProducts.at(this.productIndex) || {}
     },
-    finished() {
-      return !this.nextStorageAreaId
+    allAreasCompleted() {
+      return this.storageAreas.length === this.isAreaCompleteds.length
+    },
+    noneAreaCompleted() {
+      return this.isAreaCompleteds.length === 0
     },
   },
   methods: {
-    finishArea() {
-      this.completedStorageAreaIds.push(this.currentStorageArea.id)
-      this.nextStorageAreaId = this.nextStorageAreas.at(0)?.id
-      this.storageState = 'chooseNext'
+    startArea(area) {
+      this.currentStorageArea = area
+      this.productIndex = 0
+      this.focusStockInput()
+    },
+    finishCurrentArea() {
+      this.inventory.completed_storage_areas_ids.push(this.currentStorageArea.id)
+      this.currentStorageArea = null
+    },
+    isAreaCompleted(area) {
+      return this.inventory.completed_storage_areas_ids.includes(area.id)
+    },
+    productsForArea(area) {
+      return this.products.filter((product) => {
+        if (area.id === 'other') return product.storage_area_ids.length === 0
+        return product.storage_area_ids.includes(area.id)
+      })
+    },
+    completedProductsForArea(area) {
+      return this.productsForArea(area).filter((product) => this.inventory.values[product.id]?.value)
     },
     async onInputStockKeyEnter() {
       await this.$nextTick()
@@ -184,20 +192,13 @@ export default {
     },
   },
   watch: {
+    // initialize values for each product
     products() {
       if (!this.products || this.products.length == 0) return
-
       this.products.forEach((product) => { this.inventory.values[product.id] ||= { unit: product.unit } })
-    },
-    currentStorageAreaId() {
-      this.storageState = 'inProgress'
-      this.productIndex = 0
     },
     async currentProduct() {
       this.focusStockInput()
-    },
-    async storageState() {
-      if (this.storageState === 'inProgress') this.focusStockInput()
     },
   },
 }
@@ -211,4 +212,21 @@ export default {
 }
 
 .footer {}
+
+.storage-area-card {
+  background-color: white;
+  text-align: left;
+
+  &:hover {
+    background-color: var(--primary-color);
+    color: white;
+    // background-color: var(--bluegray-100);
+    // font-weight: bold;
+    cursor: pointer;
+  }
+
+  &.completed {
+    opacity: .5;
+  }
+}
 </style>
