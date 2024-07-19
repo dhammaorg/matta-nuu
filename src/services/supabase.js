7493416643354d0287b3876a281abd73e80542d7
Alexis <alexis.molle@protonmail.com>
2024-07-19 11:49:03 +0200

    supabase URL and ANON_KEY in an .env file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://yrurtmpzqkbrrhzaylcg.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTQ3NzIzNSwiZXhwIjoxOTU1MDUzMjM1fQ.06nzTqNHDLk0yGyk_D3wgjvHJeEZXpHQ26Rct36kcq0'
+const supabaseUrl = process.env.VUE_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.VUE_APP_SUPABASE_ANON_KEY
 const options = {
   schema: 'public',
   headers: { 'x-my-custom-header': 'my-app-name' },
